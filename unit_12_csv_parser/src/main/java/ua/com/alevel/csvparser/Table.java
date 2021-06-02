e1ffdb3403ce2347c5e0cf47785810026e1447f0
Mykhail Kushyk <mykhail.kushik@gmail.com>
2021-06-02 19:06:40 +0300

    Refactor task 12

--- unit_12_csv_parser/src/main/java/ua/com/alevel/csvparser/Table.java
+++ unit_12_csv_parser/src/main/java/ua/com/alevel/csvparser/Table.java
@@ -1,19 +1,21 @@
 package ua.com.alevel.csvparser;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class Table {
 
     private List<String> headers = new ArrayList<>();
     private List<String[]> records = new ArrayList<>();
+    private Map<String, Integer> indexesOfHeaders = new HashMap<>();
 
     public Table(List<String[]> linesFromCsv) {
         String[] stringsHeaders = linesFromCsv.get(0);
         headers.addAll(Arrays.asList(stringsHeaders));
         for (int i = 1; i < linesFromCsv.size(); i++) {
             records.add(linesFromCsv.get(i));
+        }
+        for (int i = 0; i < headers.size(); i++) {
+            indexesOfHeaders.put(headers.get(i), i);
         }
     }
 
@@ -28,15 +30,6 @@ public class Table {
 
     public String get(int row, String headerName) {
         String[] record = records.get(--row);
-        return record[getIndexByHeaderName(headerName)];
-    }
-
-    private int getIndexByHeaderName(String name) {
-        for (int i = 0; i < headers.size(); i++) {
-            if (headers.get(i).equals(name)) {
-                return i;
-            }
-        }
-        return -1;
+        return record[indexesOfHeaders.get(headerName)];
     }
 }
